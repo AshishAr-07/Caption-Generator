fa5f7dc25cebd2abcee01c3cf801441478550756
AshishAr-07 <rawatzon@gmail.com>
2026-05-22 17:26:14 +0530

    mongodb databse connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { connectDB } from "./utils/dbconfig.js";
 
 dotenv.config();
 
@@ -16,6 +17,15 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+connectDB().then(()=>{
+    try {
+        app.listen(PORT, ()=> {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error, "Mongodb Connection Failed")
+    }
+})
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
